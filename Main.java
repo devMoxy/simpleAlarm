e49673f88b351217fce2c23a35eb98081cb49664
devMoxy <adelowoenoch1@gmail.com>
2026-06-17 19:49:25 +0100

    Refactor alarm time and title handling

--- Main.java
+++ Main.java
@@ -1,32 +1,28 @@
-import javax.sound.sampled.*;
 import java.util.*;
 
 public class Main {
-
-    public static String filePath  = "src\\alarm.wav";
-    public static String alarmTitle;
-    public static String alarmTime;
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         System.out.print("Enter your alarm time in this format(HH:mm): ");
-        alarmTime = scanner.nextLine().trim();
-        System.out.print("what is the title of your alarm: ");
-        alarmTitle = scanner.nextLine().trim().toUpperCase();
+        String time = scanner.nextLine().trim();
 
-        Alarm alarm = new Alarm(alarmTitle, filePath, alarmTime);
-        AlarmPlayer audio = new AlarmPlayer();
+        System.out.print("what is the title of your alarm: ");
+        String title = scanner.nextLine().trim().toUpperCase();
+
+        Alarm alarm = new Alarm(title, "src\\alarm.wav", time);
+        AlarmPlayer audio = new AlarmPlayer(alarm, scanner);
         Time timer = new Time();
 
         while(true){
             timer.time();
-            if(alarmTime.equals(timer.realTime)){
+            if(time.equals(timer.realTime)){
                 break;
             }
         }
 
-        System.out.println("\n" + alarmTitle + "!!!");
+        System.out.println("\n" + alarm.getAlarmTitle() + "!!!");
         System.out.println("Alarm will run for 30 seconds only \nclick Enter key to stop alarm");
         audio.play();
     }
