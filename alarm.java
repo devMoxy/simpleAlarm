f725aaad95ee6a80d032da29c89bd45127ea10b6
devMoxy <adelowoenoch1@gmail.com>
2026-06-17 19:49:47 +0100

    Change alarm properties to private and final

--- alarm.java
+++ alarm.java
@@ -1,13 +1,25 @@
 import java.util.Scanner;
 
 public class Alarm {
-    public String filePath  = "src\\alarm.wav";
-    public String alarmTitle;
-    public String alarmTime;
+    private String filePath  = "src\\alarm.wav";
+    private final String alarmTitle;
+    private final String alarmTime;
 
     public Alarm(String alarmTitle, String filePath, String alarmTime){
         this.alarmTitle = alarmTitle;
         this.filePath = filePath;
         this.alarmTime = alarmTime;
+    }
+
+    public String getFilePath(){
+        return filePath;
+    }
+
+    public String getAlarmTime(){
+        return alarmTime;
+    }
+
+    public String getAlarmTitle(){
+        return alarmTitle;
     }
 }
